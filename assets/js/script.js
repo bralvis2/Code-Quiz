f9fe3949cd5221f22f9720008c9a9d2ec6b0f7e9
Balvis <107074621+bralvis2@users.noreply.github.com>
2022-07-01 19:50:28 -0500

    Second Commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/* code quiz js
+
+landing page with start button
+when start button is clicked remove welcome msg and append question 1
+    addEvent listener to button
+        activity 12
+    start timer
+    user answers question read chosen element, data value, compare to answer key.
+    if correct next question
+    if incorrect subtract 10 seconds from timer
+    end quiz
+        add up user score 
+        display input for my user to type their name
+        second html and disply user name and score - read object from local storage/activity 24*/
+
+        
