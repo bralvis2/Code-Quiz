f78eb6bb5a3fa0c1e4dff4cc6f41038c77b9930a
Balvis <107074621+bralvis2@users.noreply.github.com>
2022-07-05 12:55:38 -0500

    Fourth Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,64 +14,152 @@ when start button is clicked remove welcome msg and append question 1
         display input for my user to type their name
         second html and disply user name and score - read object from local storage/activity 24*/
 
+// Array storing 10 Quiz Questions, answer choices, and correct asnwer. 
+let questions = [
+    {
+        title: 'Commonly used data types DO Not Include:',
+        alternatives: ['Strings', 'Booleans', 'Alerts', 'Numbers',],
+        correctAnswer: 'Alerts'
+    },
+    {
+        title: 'The condition in an if/else statement is enclosed with __________.',
+        alternatives: ['quotes', 'curly brackets', 'parenthesis', 'square brackets',],
+        correctAnswer: 'curly brackets'
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store',
+        alternatives: ['numbers and strings', 'other arrays', 'booleans', 'all of the above',],
+        correctAnswer: 'all of the above'
+    },
+    {
+        title: 'String values must be enclosed within ________ when being assigned to variables.',
+        alternatives: ['commas', 'curly brackets' , 'quotes', 'parenthesis',],
+        correctAnswer: 'quotes'
+    },
+    {
+        title: 'A very useful tool used during developent and debugging for printing content to the debugger is:',
+        alternatives: ['JavaScript','terminal/bash', 'for loops', 'console.log',],
+        correctAnswer: 'console.log'
+    },
+    {
+        title: 'What is JavaScript?',
+        alternatives: ['programing language', 'HTML tags', 'CSS', 'A web page',],
+        correctAnswer: 'programing language'
+    },
+    {
+        title: 'JavaScript can:',
+        alternatives: ['Interact with HTML', 'Can make your web pages dynamic', 'Check or validate user input', 'All of the above',],
+        correctAnswer:  'All of the above'
+    },
+    {
+        title: 'A variable is used to:',
+        alternatives: ['Store a value in memory', 'Store value that does not change', 'Draw a circle', 'Draw a rectangle',],
+        correctAnswer: 'Store a value in memory'
+    },
+    {
+        title: 'A string is used for storing:',
+        alternatives: ['Numbers', 'Images', 'Text', 'Integers',],
+        correctAnswer: 'Text'
+    },
+    {
+        title: 'JavaScript uses the _________ keyword to define variables.',
+        alternatives: ['vrb', 'var', 'vari', 'va',],
+        correctAnswer: 'var'
+    },
+]
+
+// Targeting HTML Elements
+var header = document.querySelector('#header');
 var startButton = document.querySelector(".start-button");
 var hideMain = document.querySelector('main');
-var questionEl = document.querySelector('.questionCard');
-// Created the question 
-var question = document.createElement('h2');
-// created the question answer choice buttons
-var answer1 = document.createElement('button');
-var answer2 = document.createElement('button');
-var answer3 = document.createElement('button');
-var answer4 = document.createElement('button');
-
-function format(){
-    // // I appended the text/buttons
-    questionEl.appendChild(question);
-    questionEl.appendChild(answer1);
-    questionEl.appendChild(answer2);
-    questionEl.appendChild(answer3);
-    questionEl.appendChild(answer4);
-   
-    // This adds style to elements
-    question.setAttribute('style', 'font-size: 2em; text-align: center; padding: 10%');
-    answer1.setAttribute('style', 'background-color: #c2bbf0; border-radius: 8px; width: 50%; align-self: center; cursor: pointer; font-size: 1.5em; letter-spacing: 0.21px; padding: 10px');
-    answer2.setAttribute('style', 'background-color: #c2bbf0; border-radius: 8px; width: 50%; align-self: center; cursor: pointer; font-size: 1.5em; letter-spacing: 0.21px; padding: 10px');
-    answer3.setAttribute('style', 'background-color: #c2bbf0; border-radius: 8px; width: 50%; align-self: center; cursor: pointer; font-size: 1.5em; letter-spacing: 0.21px; padding: 10px');
-    answer4.setAttribute('style', 'background-color: #c2bbf0; border-radius: 8px; width: 50%; align-self: center; cursor: pointer; font-size: 1.5em; letter-spacing: 0.21px; padding: 10px');
-   }
+var quizEl = document.querySelector('.questionCard');
+var titleEl = document.querySelector('.title');
+var alts = document.querySelector('.alternatives');
+var endEl = document.querySelector('.end');
+var btnA = document.querySelector('#a');
+var btnB = document.querySelector('#b');
+var btnC = document.querySelector('#c');
+var btnD = document.querySelector('#d');
+var feedback = document.querySelector('#feedback-text')
 
 
+
+// Global variables
+var finalQuestionIndex = questions.length;
+var currentQuestionIndex = 0;
+var timeRemaining = 100;
+var timerInterval;
+var score = 0;
+var correct;
+
+
+// This will hide the start game elements and display the questions.
 function startQuiz(){
     hideMain.style.display = 'none';
-    question1();
 }
 
+// Hides the Start main element of the HTML
 startButton.addEventListener('click', startQuiz);
 
-function question1(){
-    //I added text to the buttons
-    question.textContent = 'Question 1: Commonly used data types DO Not Include:';
-    answer1.textContent = 'Strings';
-    answer2.textContent = 'Booleans';
-    answer3.textContent = 'Alerts';
-    answer4.textContent = 'Numbers';
-    format();
-    
-    if(answer3.clicked == true){
-        alert("button was clicked")
-    }
+
+// Populates the quiz with the 10 question, answer choices, and correct answer Array
+function showQuizQuestions(){
+    if (currentQuestionIndex === finalQuestionIndex){
+        return  endQuiz();
     
     }
+    let currentQuestion = questions[currentQuestionIndex];
+    titleEl.innerHTML = currentQuestion.title;
+    btnA.innerHTML = currentQuestion.alternatives[0];
+    btnB.innerHTML = currentQuestion.alternatives[1];
+    btnC.innerHTML = currentQuestion.alternatives[2];
+    btnD.innerHTML = currentQuestion.alternatives[3];
+    };
 
-   
+function checkAnswers(event){
+    let correct = questions[currentQuestionIndex].correctAnswer;
 
-function question2(){
-    //I added text to the buttons
-    question.textContent = 'Question 2: The condition in an if / else statement is enclosed with ___________.';
-    answer1.textContent = 'Quotes';
-    answer2.textContent = 'Curly brackets';
-    answer3.textContent = 'Parenthesis';
-    answer4.textContent = 'Square brackets';
-    format();
-   }
+    if (event === correct){
+        feedback.innerHTML = 'Correct!'
+        currentQuestionIndex++;
+        showQuizQuestions();
+    }else{
+        feedback.innerHTML = 'Incorrect'
+        currentQuestionIndex++;
+        showQuizQuestions();
+    }
+}
+
+function endQuiz(){
+    endEl.style.display = 'block';
+    quizEl.style.display= 'none';
+    header.style.display='none';
+
+}
+// function answerChoices (answer){
+//     correct = questions[currentQuestionIndex].correctAnswer;
+    
+//     if(answer === correct && currentQuestionIndex !== finalQuestionIndex){
+//         score++;
+//         feedback.innerHTML = 'Correct!';
+//         currentQuestionIndex++;
+//         showQuizQuestions();
+//     }else {
+//         score--;
+//         feedback.innerHTML = 'Incorrect';
+//         currentQuestionIndex++;
+//         showQuizQuestions();
+//     }
+      
+//     }
+
+
+// This will hide the start game elements and display the questions.
+function startQuiz(){
+    hideMain.style.display = 'none';
+    quizEl.style.display = 'block';
+    showQuizQuestions();
+};
+
+// Hides the Start main element of the HTML
+startButton.addEventListener('click', startQuiz);
